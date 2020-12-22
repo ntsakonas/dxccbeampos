ad1d05e4c003a2b0035d6a5000af93e02a813dd8
ntsakonas <magicnickus@yahoo.com>
2020-12-22 00:08:26 +0000

    added licensing information

--- src/main/java/ntsakonas/dxccbeampos/Main.java
+++ src/main/java/ntsakonas/dxccbeampos/Main.java
@@ -1,3 +1,23 @@
+/*
+    DXCC beaming position: a utility to quickly assess your positioning around the beaming of a DX station
+     using their current QSOs with another station as an indicator.
+
+    Copyright (C) 2020  Nick Tsakonas, SV1DJG/2E0PZA
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package ntsakonas.dxccbeampos;
 
 import java.io.BufferedReader;
@@ -20,7 +40,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("DXCC Beaming calculator v1.0 (2020), SV1DJG/2E0PZA");
+        System.out.println("DXCC Beaming calculator v1.0 (Copyright (C) 2020, SV1DJG/2E0PZA)");
+        System.out.println("This program comes with ABSOLUTELY NO WARRANTY; ");
+        System.out.println("This is free software, and you are welcome to redistribute it\n" +
+                "    under certain conditions; ");
+        System.out.println();
+
         if (args.length != 1) {
             System.out.println("You need to provide your own DXCC prefix that will be used as the reference location.");
             return;
